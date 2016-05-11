0586be094adbb1e633419f0a41530133bcf4a75d
Achilles Xu <formalin14@gmail.com>
2016-05-11 22:11:32 +0800

    正在做登录

--- lib/yapc2016.pm
+++ lib/yapc2016.pm
@@ -1,10 +1,12 @@
 package yapc2016;
 use Dancer ':syntax';
+use DB;
+use Check;
 
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index';
+    redirect '/c/2016';
 };
 
 get '/halt' => sub {
@@ -13,6 +15,14 @@ get '/halt' => sub {
 
 get '/error' => sub {
     send_error "hello2", 400;
+};
+
+get '/c/:conferenceName' => sub {
+    my $conferenceName = param 'conferenceName';
+    check_required($conferenceName);
+    my $conference = db->coll('conferences')->find_one({name => $conferenceName});
+    check_required($conference);
+    template 'conference_home' => { page_title => '首页', page_channel => 'home', item => $conference };
 };
 
 true;
